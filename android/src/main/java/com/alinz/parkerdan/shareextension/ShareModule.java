4c3de93e2e45f04a287db34837d9629b359cb5d1
Tetiana Marych <tetiana.marych@vilmate.com>
2020-01-31 18:49:43 +0200

    rewriting logic for sharing for android

--- android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
+++ android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
@@ -1,98 +1,102 @@
 package com.alinz.parkerdan.shareextension;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.Arguments;
 
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.widget.Toast;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 
-import android.graphics.Bitmap;
-import android.util.Log;
-import java.io.InputStream;
-
-
 public class ShareModule extends ReactContextBaseJavaModule {
 
+  private final ReactApplicationContext reactContext;
 
   public ShareModule(ReactApplicationContext reactContext) {
-      super(reactContext);
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  protected void onNewIntent(Intent intent) {
+    Activity mActivity = getCurrentActivity();
+    
+    if(mActivity == null) { return; }
+
+    mActivity.setIntent(intent);
+  }
+
+  @ReactMethod
+  public void getFilepath(Callback successCallback) {
+    Activity mActivity = getCurrentActivity();
+    
+    if(mActivity == null) { return; }
+    
+    Intent intent = mActivity.getIntent();
+    String action = intent.getAction();
+    String type = intent.getType();
+
+    if (Intent.ACTION_SEND.equals(action) && type != null) {
+      if ("text/plain".equals(type)) {
+        String input = intent.getStringExtra(Intent.EXTRA_TEXT);
+        WritableArray promiseArray = Arguments.createArray();
+        promiseArray.pushString(input);
+        successCallback.invoke(promiseArray);
+      } else if (type.startsWith("image/") || type.startsWith("application/")) {
+        Uri fileUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (fileUri != null) {
+          String resUri = "file://" + RealPathUtil.getRealPathFromURI(mActivity, fileUri);
+          WritableArray promiseArray = Arguments.createArray();
+          promiseArray.pushString(resUri);
+          successCallback.invoke(promiseArray);
+        }
+      }else {
+        Toast.makeText(reactContext, "Type is not support", Toast.LENGTH_SHORT).show();
+      }
+    } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+        if (type.startsWith("image/") || type.startsWith("application/")) {
+          ArrayList<Uri> fileUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+          if (fileUris != null) {
+            WritableArray promiseArray = Arguments.createArray();
+            for (Uri uri: fileUris) {
+              String resUri = "file://" + RealPathUtil.getRealPathFromURI(mActivity, uri);
+              promiseArray.pushString(resUri);
+            }
+            successCallback.invoke(promiseArray);
+          }
+        } else {
+          Toast.makeText(reactContext, "Type is not support", Toast.LENGTH_SHORT).show();
+        }
+    }
+  }
+
+  @ReactMethod
+  public void clearFilePath() {
+    // TODO: do we really need it?
+    Activity mActivity = getCurrentActivity();
+    
+    if(mActivity == null) { return; }
+
+    Intent intent = mActivity.getIntent();
+    String type = intent.getType();
+    if ("text/plain".equals(type)) {
+      intent.removeExtra(Intent.EXTRA_TEXT);
+    } else if (type.startsWith("image/") || type.startsWith("video/") || type.startsWith("application/")) {
+      intent.removeExtra(Intent.EXTRA_STREAM);
+    }
   }
 
   @Override
   public String getName() {
-      return "ReactNativeShareExtension";
-  }
-
-  @ReactMethod
-  public void close() {
-    Activity currentActivity = getCurrentActivity();
-
-    if (currentActivity != null) {
-      currentActivity.finish();
-    } else {
-        Log.e("Error: ", "Activity not found");
-    }
-  }
- 
-    
-  @ReactMethod
-  public void openURL(String url) {
-      Uri uri = Uri.parse(url);
-      Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-
-      Activity currentActivity = getCurrentActivity();
-
-      if (currentActivity != null) {
-        currentActivity.startActivity(intent);
-      } else {
-        Log.e("Error: ", "Activity not found");
-      }
-  }
-
-  @ReactMethod
-  public void data(Promise promise) {
-      promise.resolve(processIntent());
-  }
-
-  public WritableMap processIntent() {
-      WritableMap map = Arguments.createMap();
-
-      String value = "";
-      String type = "";
-      String action = "";
-
-      Activity currentActivity = getCurrentActivity();
-
-      if (currentActivity != null) {
-        Intent intent = currentActivity.getIntent();
-        action = intent.getAction();
-        type = intent.getType();
-        if (type == null) {
-          type = "";
-        }
-        if (Intent.ACTION_SEND.equals(action) && "text/plain".equals(type)) {
-          value = intent.getStringExtra(Intent.EXTRA_TEXT);
-        }
-        else if (Intent.ACTION_SEND.equals(action) && ("image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type) ) ) {
-          Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-         value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
-
-       } else {
-         value = "";
-       }
-      } else {
-        value = "";
-        type = "";
-      }
-
-      map.putString("type", type);
-      map.putString("value",value);
-
-      return map;
+    return "ReactNativeShareExtension";
   }
 }
